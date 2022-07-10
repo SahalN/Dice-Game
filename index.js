90a2edca2ef600df526d04e1e0db3baf36b3f400
Muhammad Sahal Nurdin <94037920+SahalN@users.noreply.github.com>
2022-07-10 14:45:42 +0700

    Final Commit!

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+// Untuk random number satu sampai 6
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; // 1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; // dice1.png - dice6.png
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6)  + 1;
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+// Player1 menang!
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 Menang!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 menang!";
+}
+else {
+  document.querySelector("h1").innerHTML = "Imbang!";
+}
